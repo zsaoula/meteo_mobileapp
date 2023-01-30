e19a628f3e357119478e9905210dc171ffd9a9e6
zakariya.saoula <zakariya.saoula@etu.uca.fr>
2023-01-30 20:11:22 +0100

    style affichage d'une ville

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { WeatherList } from './compenent/allWeathers';
 import { CityDisplay } from './compenent/weatherCity';
 import {colorBackground} from './styles/style';
+import {FAVORITE_CITY_DATA} from './data/stub';
 
 export default function App() {
   return (
@@ -15,8 +16,9 @@ export default function App() {
         <Text>ZZ</Text>
       </View>
       <View>
-        <WeatherList></WeatherList>
-        {/* <CityDisplay name={'Clermont-Ferrand'}></CityDisplay> */}
+        {/* <WeatherList></WeatherList> */}
+        <CityDisplay name={'Lyon'}></CityDisplay>
+        {/* <CityDisplay name={FAVORITE_CITY_DATA.name}></CityDisplay> */}
       </View>
     </View>
   );
--- compenent/weatherCity.tsx
+++ compenent/weatherCity.tsx
@@ -1,5 +1,5 @@
 import {WEATHER_DATA} from "../data/stub";
-import { View,Text, StyleSheet, FlatList } from "react-native";
+import { View,Text, StyleSheet, Image } from "react-native";
 import {cityName} from "../styles/style";
 
 type cityProps = {
@@ -7,34 +7,59 @@ type cityProps = {
 }
 
 export function CityDisplay(props: cityProps) {
-      const allWeathers = WEATHER_DATA.filter(weather => weather.city.name === props.name)
+      const cityWeather = WEATHER_DATA.filter(weather => weather.city.name === props.name)[0]
+      const dateCity = cityWeather.at.split(" ")[0]
+      const timeCity = cityWeather.at.split(" ")[1].slice(0,5)
       return (
-        <View>
-        <Text>{allWeathers[0].city.name}</Text>
+        <View style={weatherCityStyle.container}>
+          <View style={weatherCityStyle.containerPosition}>
+          <Text style={weatherCityStyle.longAndLatt}>{cityWeather.city.latitude}</Text>
+          <Text style={weatherCityStyle.longAndLatt}>{cityWeather.city.longitude}</Text>
+          </View>
+        <Text style={weatherCityStyle.name}>{cityWeather.city.name}</Text>
+        <Text>{dateCity}</Text>
+        <Text>{timeCity}</Text>
+        <Image source={require('../assets/image/nuage.png')} style={weatherCityStyle.image}></Image>
+        {/* avec weather type -> img */}
+        <Text style={weatherCityStyle.temp}>{Math.floor(cityWeather.temperature)}°C</Text>
+        <Text style={weatherCityStyle.desc}>{cityWeather.weatherDescription}</Text>
       </View>
     );
 };
 
-const weatherListStyle = StyleSheet.create({
+const weatherCityStyle = StyleSheet.create({
     container: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        borderWidth: 1,
-        backgroundColor: 'white',
-        borderColor: "white",
-        padding: 10,
-        margin: 5,
-        borderRadius: 5
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%"
     },
     name: {
       color: cityName,
-      fontSize: 15,
+      fontSize: 30,
       fontWeight: "bold"
     },
-
-    temperature: {
+    temp: {
       color: cityName,
-      fontSize: 12
+      fontSize: 65,
+      fontWeight: "bold"
+    },
+    containerPosition: {
+      flexDirection: "row",
+      justifyContent:"space-around",
+      width: "50%"
+    },
+    longAndLatt: {
+      fontSize: 13,
+      fontWeight: "100"
+    },
+    image: {
+      width: 150,
+      height: 150
+    },
+    desc: {
+      fontSize:20,
+      fontWeight: "300"
     }
 
 });
